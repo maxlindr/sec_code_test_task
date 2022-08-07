a0ddb15564d420ac7c6a660aa4e952c4e1624a7f
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2022-08-07 19:20:22 +0300

    Start page

---
+++ src/views/StartPage/StartPage.tsx
@@ -0,0 +1,9 @@
+import styles from './StartPage.module.scss';
+
+export const StartPage = () => {
+  return (
+    <div className={styles.root}>
+      <button className={styles.btn}>Load</button>
+    </div>
+  );
+};
